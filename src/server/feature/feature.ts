9db57989262f0144b08cd65c63759666ea50be4d
dgstephenson <dgstephenson.econ@gmail.com>
2024-07-27 17:19:39 -0400

    getContacts

--- src/server/feature/feature.ts
+++ src/server/feature/feature.ts
@@ -1,4 +1,4 @@
-import { AABB, Body, BodyDef, Fixture, FixtureDef, CircleShape, Vec2, PolygonShape } from 'planck'
+import { AABB, Body, BodyDef, Fixture, FixtureDef, CircleShape, Vec2, PolygonShape, Contact } from 'planck'
 import { Color } from '../../shared/color'
 import { Actor } from '../actor/actor'
 // import { DebugLine } from '../../shared/debugLine'
@@ -43,6 +43,19 @@ export class Feature {
     this.borderWidth = props.borderWidth ?? 0.1
     actorCount += 1
     this.id = actorCount
+  }
+
+  getContacts (): Contact[] {
+    const contactEdges = []
+    for (
+      let contactEdge = this.body.getContactList();
+      contactEdge != null;
+      contactEdge = contactEdge.next
+    ) {
+      contactEdges.push(contactEdge)
+    }
+    const contacts = contactEdges.map(contactEdge => contactEdge.contact)
+    return contacts
   }
 
   getFeaturesInRange (): Feature[] {
