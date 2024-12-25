9fb296888a7712874b7c87edb232a25262d1df2b
David Y. Stephenson <david@davidystephenson.com>
2024-12-25 12:47:22 -0500

    Refactor logging

--- src/server/stage/dressRehearsal.ts
+++ src/server/stage/dressRehearsal.ts
@@ -6,33 +6,31 @@ export class DressRehearsal extends Procedural {
   constructor () {
     super({
       flags: new Flags({
-        navAreas: false,
-        performance: false,
-        respawn: false,
-        hungerY: false,
+        // performance: false,
         visionY: false,
         // botChase: true,
         // botPath: true,
-        controlLines: true
-        // charge: true
+        controlLines: true,
+        // charge: true,
+        timings: true
       }),
-      halfHeight: 20,
-      halfWidth: 20
+      halfHeight: 50,
+      halfWidth: 50
     })
 
     this.navigation.setupWaypoints()
     this.spawner.setupSpawnPoints()
 
-    // this.addBalanced({ position: Vec2(5, 5) })
+    this.addBalanced({ position: Vec2(5, 5) })
     this.addBully({ position: Vec2(-5, 5) })
-    // this.addHunter({ position: Vec2(5, -5) })
-    // this.addScavenger({ position: Vec2(5, -5) })
-    // this.addStamina({ position: Vec2(-5, -5) })
-    // this.addStrength({ position: Vec2(0, 5) })
-    // this.addSpeed({ position: Vec2(5, 0) })
-    // this.addTrapper({ position: Vec2(0, -5) })
+    this.addHunter({ position: Vec2(5, -5) })
+    this.addScavenger({ position: Vec2(5, -5) })
+    this.addStamina({ position: Vec2(-5, -5) })
+    this.addStrength({ position: Vec2(0, 5) })
+    this.addSpeed({ position: Vec2(5, 0) })
+    this.addTrapper({ position: Vec2(0, -5) })
 
-    // this.addTree({ position: Vec2(20, -20) })
+    this.addTree({ position: Vec2(0, 0) })
     // this.addTree({ position: Vec2(-20, -20) })
     // this.addTree({ position: Vec2(-20, 20) })
   }
