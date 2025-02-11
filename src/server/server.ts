d2ac8209462347de9763607170191c3732eb32d2
danielgstephenson <dgstephenson.econ@gmail.com>
2025-02-11 16:01:27 -0500

    server class

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,17 +1,106 @@
+import { Vec2 } from 'planck'
+import { Controls } from '../shared/input'
+// import { Funhouse } from './funhouse'
+import { GREEN } from '../shared/color'
+// import { PublicProduction } from './stage/publicProduction'
+// import { GrandRehearsal } from './stage/grandRehearsal'
+// import { Rehearsal } from './stage/rehearsal'
+// import { Mission } from './stage/mission'
+// import { TestProduction } from './stage/testProduction'
+// import { Rehearsal } from './stage/rehearsal'
+// import { GrandRehearsal } from './stage/grandRehearsal'
+// import { PublicProduction } from './stage/publicProduction'
+// import { DressRehearsal } from './stage/dressRehearsal'
+import { PublicProduction } from './stage/publicProduction'
+// import { PrivateProduction } from './stage/privateProduction'
+
 import express from 'express'
-import path from 'path'
 import http from 'http'
-import { Server } from 'socket.io'
+import https from 'https'
+import fs from 'fs-extra'
+import path from 'path'
+import * as SocketIo from 'socket.io'
+import { Config } from './config'
+import { Playhouse } from './stage/playhouse'
 
-const app = express()
-const staticPath = path.join(__dirname, '..', '..', 'dist')
-const staticMiddleware = express.static(staticPath)
-app.use(staticMiddleware)
-const server = new http.Server(app)
-const PORT = process.env.PORT ?? 3000
-console.info('PORT set as', PORT)
-server.listen(PORT, () => {
-  console.info(`Listening on :${PORT}`)
-})
+export class Server {
+  seed = Math.random().toString()
+  config = new Config()
+  dirname = path.dirname(__filename)
+  app = express()
+  httpServer: https.Server | http.Server
+  io: SocketIoServer
+  playhouse: Playhouse
+  step = 0
 
-export const io = new Server(server)
+  constructor () {
+    this.setupApp()
+    this.httpServer = this.getHttpServer()
+    this.io = new SocketIo.Server(this.httpServer)
+    this.playhouse = new PublicProduction()
+    void this.start()
+  }
+
+  async start (): Promise<void> {
+    this.httpServer.listen(this.config.port, () => {
+      console.log(`listening on port: ${this.config.port}`)
+    })
+    this.io.on('connection', socket => {
+      this.playhouse.debug({ vs: ['connection:', socket.id] })
+      socket.emit('connected')
+      const player = this.playhouse.addPlayer({
+        color: GREEN,
+        id: socket.id,
+        gene: this.playhouse.balancedGene,
+        position: Vec2(0, 0)
+      })
+      if (player.organism == null) {
+        throw new Error('player.organism is undefined')
+      }
+      player.organism.membrane.hungerDamage = 0.5
+      // player.organism.membrane.combatDamage = 0.9
+      socket.on('controls', (controls: Controls) => {
+        if (player.organism != null) {
+          player.organism.controls = controls
+          if (controls.select) {
+            this.playhouse.runner.paused = true
+          }
+          if (controls.cancel) {
+            this.playhouse.runner.paused = false
+            // player.organism.membrane.combatDamage = 0
+            // player.organism.membrane.hungerDamage = 0
+          }
+        }
+        const summary = this.playhouse.runner.getSummary({ player })
+        socket.emit('serverUpdateClient', summary)
+      })
+      socket.on('disconnect', () => {
+        this.playhouse.debug({ vs: ['disconnect:', socket.id] })
+        player.organism?.destroy()
+      })
+    })
+  }
+
+  setupApp (): void {
+    const staticPath = path.join(this.dirname, 'public')
+    const staticMiddleware = express.static(staticPath)
+    this.app.use(staticMiddleware)
+    const clientHtmlPath = path.join(this.dirname, 'public', 'client.html')
+    this.app.get('/', function (req, res) { res.sendFile(clientHtmlPath) })
+  }
+
+  getHttpServer (): https.Server | http.Server {
+    if (this.config.secure) {
+      const keyPath = path.join(this.dirname, '../sis-key.pem')
+      const certPath = path.join(this.dirname, '../sis-cert.pem')
+      const key = fs.readFileSync(keyPath)
+      const cert = fs.readFileSync(certPath)
+      const credentials = { key, cert }
+      return new https.Server(credentials, this.app)
+    } else {
+      return new http.Server(this.app)
+    }
+  }
+}
+
+type SocketIoServer = SocketIo.Server
