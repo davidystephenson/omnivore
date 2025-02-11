2371974b0d5ad3c1763266c9c5fa6a7fefdece54
danielgstephenson <dgstephenson.econ@gmail.com>
2025-02-11 16:01:27 -0500

    server class

---
+++ src/server/config.ts
@@ -0,0 +1,20 @@
+import fs from 'fs-extra'
+import path from 'path'
+
+export class Config {
+  port = 3000
+  secure = false
+
+  constructor () {
+    const dirname = path.dirname(__filename)
+    const configPath = path.join(dirname, '../../config.json')
+    const fileExists: boolean = fs.existsSync(configPath)
+    if (fileExists) {
+      const json = fs.readJSONSync(configPath)
+      if (typeof json.port === 'number') this.port = json.port
+      if (typeof json.secure === 'boolean') this.secure = json.secure
+    }
+    console.log('port:', this.port)
+    console.log('secure:', this.secure)
+  }
+}
