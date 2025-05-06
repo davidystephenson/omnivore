7797ed5fde19430cc584bb18d1fa522de825f073
danielgstephenson <dgstephenson.econ@gmail.com>
2025-05-06 17:40:12 -0400

    update

--- src/server/actor/organism.ts
+++ src/server/actor/organism.ts
@@ -27,8 +27,8 @@ export interface Obituary extends OrganismSpawn {
 }
 
 export class Organism extends Actor {
-  static GENETIC_FORCE_SCALE = 0.25
-  static MINIMUM_FORCE = 5
+  static GENETIC_FORCE_SCALE = 0.6
+  static MINIMUM_FORCE = 2.5
   controlColor = LIME
   chasePoint: Vec2 | undefined
   chaseRadius = 0.2
--- src/server/gene.ts
+++ src/server/gene.ts
@@ -128,7 +128,7 @@ export class Gene {
     this.debug({ k: 'getMutated value', v: value })
     const remaining = 1 - value
     this.debug({ k: 'getMutated remaining', v: remaining })
-    const random = Math.random() * 1
+    const random = Math.random() * 0.5
     this.debug({ k: 'getMutated random', v: random })
     const others = STATS.filter(stat => stat !== props.stat)
     this.debug({ k: 'getMutated others', v: others })
