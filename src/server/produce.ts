c7093b967b88f9996aa6502795542d926352ad6e
David Y. Stephenson <david@davidystephenson.com>
2025-05-07 18:59:06 -0400

    Log promptbook

--- src/server/produce.ts
+++ src/server/produce.ts
@@ -3,23 +3,44 @@ import { Producer } from './producer'
 import fs from 'fs'
 import json from 'big-json'
 
-const readStream = fs.createReadStream('input.json')
 const parseStream = json.createParseStream()
 
-console.log('Parsing JSON...')
-parseStream.on(
-  'end',
-  (x: unknown) => {
-    console.log('JSON parsed')
-    console.log('x', x)
-  }
-)
 parseStream.on('data', function (pojo) {
-  const keys = Object.keys(pojo)
-  console.log('keys.length', keys.length)
+  console.log('JSON parsed')
   void new Producer({
     layoutData: pojo as unknown as LayoutData
   })
+})
+
+console.log('Reading promptbook...')
+const readStream = fs.createReadStream('promptbook.json')
+
+readStream.on('open', () => {
+  console.log('Promptbook opened')
+})
+
+readStream.on('close', () => {
+  console.log('Promptbook closed')
+})
+
+let index = 0
+readStream.on('data', (chunk) => {
+  if (index === 0 || index % 100 === 0) {
+    console.log('Prompt', index, 'is', chunk.length, 'long')
+  }
+  index++
+})
+
+readStream.on('ready', () => {
+  console.log('Promptbook ready')
+})
+
+readStream.on('end', () => {
+  console.log('Promptbook read')
+})
+
+readStream.on('error', (error) => {
+  console.error('Error reading promptbook', error)
 })
 
 readStream.pipe(parseStream)
