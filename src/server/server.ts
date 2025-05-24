e04a566ba00861eeaf5096e14debd79692a370d8
David Y. Stephenson <david@davidystephenson.com>
2025-05-24 16:41:05 -0400

    Remove log

--- src/server/server.ts
+++ src/server/server.ts
@@ -54,7 +54,6 @@ export class Server {
       // player.organism.membrane.hungerDamage = 0.5
       // player.organism.membrane.combatDamage = 0.9
       socket.on('controls', (controls: Controls) => {
-        console.log('player controls', socket.id)
         if (player.organism != null) {
           player.organism.controls = controls
           if (this.playhouse.flags.playerControl) {
