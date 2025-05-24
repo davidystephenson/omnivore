2510f7a80f9133a3441e51b0e00a6cd660f9692f
David Y. Stephenson <david@davidystephenson.com>
2025-05-24 16:20:03 -0400

    Increase scale

--- src/server/actor/organism.ts
+++ src/server/actor/organism.ts
@@ -31,8 +31,8 @@ export interface Obituary extends OrganismSpawn {
 export class Organism extends Actor {
   static BLOCKED_DISTANCE = 4
   static TRAPPED_DISTANCE = 0.5 * Organism.BLOCKED_DISTANCE
-  static GENETIC_FORCE_SCALE = 0.45
-  static MINIMUM_FORCE = 1.5
+  static GENETIC_FORCE_SCALE = 0.5
+  static MINIMUM_FORCE = 1
   controlColor = LIME
   chasePoint: Vec2 | undefined
   chaseRadius = 0.2
