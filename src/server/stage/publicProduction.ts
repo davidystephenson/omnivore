12e8cbe90a70d5a5fdb1a589c559621816aedcaf
David Y. Stephenson <david@davidystephenson.com>
2025-06-03 17:00:14 -0400

    Restore balance

--- src/server/stage/publicProduction.ts
+++ src/server/stage/publicProduction.ts
@@ -3,7 +3,7 @@ import { Promptbook } from '../types'
 import { Performance } from './performance'
 
 export class PublicProduction extends Performance {
-  playerGene = this.flyBullyGene
+  playerGene = this.apeGene
 
   constructor (props: {
     promptbook: Promptbook
