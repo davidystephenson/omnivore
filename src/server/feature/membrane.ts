9bd6f2d5868bc342be56bb883a0bf5d0631f234c
David Y. Stephenson <david@davidystephenson.com>
2025-06-14 14:56:31 -0400

    Decrease size factor

--- src/server/feature/membrane.ts
+++ src/server/feature/membrane.ts
@@ -105,7 +105,7 @@ export class Membrane extends Feature {
       this.damageLog({ k: 'baseDamage', v: baseDamage })
       const strengthDamage = baseDamage * Math.pow(reversed, 100)
       this.damageLog({ k: 'strengthDamage', v: strengthDamage })
-      const sizeFactor = Math.pow(reversed, 75)
+      const sizeFactor = Math.pow(reversed, 85)
       this.damageLog({ k: 'sizeFactor', v: sizeFactor })
       const sizeDamage = 0.01 * sizeFactor
       this.damageLog({ k: 'sizeDamage', v: sizeDamage })
