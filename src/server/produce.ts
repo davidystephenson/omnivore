691bb4aef57e41a89dd5f10076ed0d538a280e3d
David Y. Stephenson <david@davidystephenson.com>
2025-08-02 15:50:14 -0400

    Check before deleting

--- src/server/produce.ts
+++ src/server/produce.ts
@@ -1,14 +1,18 @@
-import { promises } from 'fs'
+import fs, { promises } from 'fs'
 import downloadPromptbook from './downloadPromptbook'
 import perform from './perform'
 import readPromptbook from './readPromptbook'
 import decompress from 'decompress'
 
 const DOWNLOAD_PATH = './promptbooks/download'
+const ZIP_PATH = `${DOWNLOAD_PATH}.zip`
 
 async function main (): Promise<void> {
   console.info('Producing...')
-  await promises.rm('./promptbooks/download.zip')
+  const zipExists = fs.existsSync(ZIP_PATH)
+  if (zipExists) {
+    await promises.rm(ZIP_PATH)
+  }
   await promises.rm(DOWNLOAD_PATH, { recursive: true, force: true })
   await promises.mkdir(DOWNLOAD_PATH, { recursive: true })
   await downloadPromptbook()
