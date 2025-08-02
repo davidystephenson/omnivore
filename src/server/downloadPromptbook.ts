3916bb218088ebee436153c3c96b865b56c8920b
David Y. Stephenson <david@davidystephenson.com>
2025-08-02 14:01:56 -0400

    Separate directory

--- src/server/downloadPromptbook.ts
+++ src/server/downloadPromptbook.ts
@@ -3,7 +3,8 @@ import { finished } from 'stream/promises'
 
 const URL = 'https://firebasestorage.googleapis.com/v0/b/playomnivore.firebasestorage.app/o/100.zip?alt=media&token=1da4edc5-6d58-4b5f-9d58-a8dc29dbe507'
 // https://drive.google.com/file/d/1e7QeIVHvBYspa_JQeu82SS1mIexB4Pey/view?usp=sharing
-const PATH = process.env.RAILWAY_VOLUME_MOUNT_PATH ?? './promptbooks/download.zip'
+const DIRECTORY = process.env.RAILWAY_VOLUME_MOUNT_PATH ?? './promptbooks'
+const PATH = `${DIRECTORY}/download.zip`
 
 export default async function downloadPromptbook (): Promise<void> {
   console.log('RAILWAY_VOLUME_MOUNT_PATH:', process.env.RAILWAY_VOLUME_MOUNT_PATH)
